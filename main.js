b940b21b0291f3a49ae029c5f9c804648d884cf0
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-25 13:26:25 +0100

    add button delete meessages

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// scegli il contatto e appare i suo messaggi, @click set visible:true v-if mostra il div
-
 const { createApp } = Vue;
 
 createApp({
@@ -17,12 +15,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '10/01/2020 16:15:22',
@@ -39,17 +39,19 @@ createApp({
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         }
                     ],
                 },
@@ -66,7 +68,8 @@ createApp({
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '28/03/2020 16:15:22',
@@ -83,7 +86,8 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '10/01/2020 15:50:00',
@@ -100,7 +104,8 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '10/01/2020 15:50:00',
@@ -117,7 +122,8 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '10/01/2020 15:50:00',
@@ -127,7 +133,8 @@ createApp({
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         }
                     ],
                 },
@@ -139,7 +146,8 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '10/01/2020 15:50:00',
@@ -161,7 +169,8 @@ createApp({
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: ''
                         },
                         {
                             date: '10/01/2020 15:51:00',
@@ -174,20 +183,21 @@ createApp({
         }
     },
     methods: {
+
         showConversation(indice) {
             this.activeItem = indice;
         },
+
         sendMessage() {
             this.contacts[this.activeItem].messages.push(
                 {
                     date: 'now',
                     message: this.inputMessage,
-                    status: 'sent'
+                    status: 'sent',
+                    dropdown: ''
                 }
             )
-
             this.inputMessage = '';
-
             setTimeout(() => {
                 this.contacts[this.activeItem].messages.push(
                     {
@@ -198,7 +208,44 @@ createApp({
                 )
             }, 1000);
         },
+
+        clikArrow(indice) {
+            if (this.contacts[this.activeItem].messages[indice].dropdown === '') {
+                this.contacts[this.activeItem].messages[indice].dropdown = 'show';
+            } else {
+                this.contacts[this.activeItem].messages[indice].dropdown = ''
+            }
+
+        },
+
+        deleteMessages(indice) {
+            const filteredSentMessages = this.contacts[this.activeItem].messages.filter((stat) => {
+                if (stat.status === 'sent') {
+                    return true
+                }
+            });
+            filteredSentMessages.pop();
+
+            const filteredReceivedMessages = this.contacts[this.activeItem].messages.filter((stat) => {
+                if (stat.status === 'received') {
+                    return true
+                }
+            });
+
+            const newArrayMessages = filteredSentMessages.concat(filteredReceivedMessages);
+
+            this.contacts[this.activeItem].messages = newArrayMessages;
+
+            if (this.contacts[this.activeItem].messages[indice].dropdown === '') {
+                this.contacts[this.activeItem].messages[indice].dropdown = 'show';
+            } else {
+                this.contacts[this.activeItem].messages[indice].dropdown = ''
+            }
+        },
+
     },
+
+
     computed: {
         filteredList() {
             return this.contacts.filter(contact => {
@@ -212,3 +259,4 @@ createApp({
 // import { DateTime } from 'luxon';
 
 // DateTime.now();
+
